2705412f317ce57976ab6f2d66d296c4e50a64d0
rinnimatthews <rinnimatthews@gmail.com>
2017-02-08 02:16:50 -0800

    Customize

--- main_dist.sh
+++ main_dist.sh
@@ -28,7 +28,7 @@ creating_custom_desktop_icons() {
 	echo "Icon=/tmp/ECC-certis/Images/CEH.png" >> CEH.desktop
 	echo "Terminal=true" >> CEH.desktop
 	echo "Type=Application" >> CEH.desktop
-	echo "Categories=Utility;Application;" >> CEH.desktop
+	echo "Categories=Application;" >> CEH.desktop
 	chmod +x CEH.desktop
 	cp -R /tmp/ECC-certis/ECC-iconfiles/CEH.desktop /usr/share/applications/
 	echo "CEH Icon created"
