eaf7046329476a93e6814ab089af4b7a85e58755
rinnimatthews <rinnimatthews@gmail.com>
2017-02-08 01:55:16 -0800

    Customize

--- main_dist.sh
+++ main_dist.sh
@@ -4,8 +4,7 @@ install_desktop_background() {
 echo "Transforming Desktop Background"
 	CDIR=$(pwd)
 	cd /tmp/
-	apt-get install git
-	apt-get install vim
+	#apt-get install git
 	git clone --recursive https://github.com/rinnimatthews/ECC-certis /tmp/ECC-certis >> $HOME/ECC-install.log 2>&1
 cp -R /tmp/ECC-certis/Images/warty-final-ubuntu.png /usr/share/backgrounds/
 
@@ -15,28 +14,28 @@ cp -R /tmp/ECC-certis/Images/warty-final-ubuntu.png /usr/share/backgrounds/
 
 # Creating custom desktop icons
 creating_custom_desktop_icons() {
-echo "Creating Custom desktop Icons"
+	echo "Creating Custom desktop Icons"
 	CDIR=$(pwd)
 	cd /tmp/ECC-certis
 	mkdir ECC-iconfiles
 	cd ECC-iconfiles
-	vi CEH.desktop
-	[Desktop Entry]
-	Version=10.0
-	Name=CEH       
-	Comment=Certified Ethical Hacker
-	Exec=/tmp/ECC-certis/link_files/CEH.sh
-	Icon=/tmp/ECC-certis/Images/CEH.png
-	Terminal=true
-	Type=Application
-	Categories=Utility;Application;
-	:wq
+	echo "Creating CEH Icon"
+	echo "[Desktop Entry]" >> CEH.desktop
+	echo "Version=10.0" >> CEH.desktop
+	echo "Name=CEH" >> CEH.desktop
+	echo "Comment=Certified Ethical Hacker" >> CEH.desktop
+	echo "Exec=/tmp/ECC-certis/link_files/CEH.sh" >> CEH.desktop
+	echo "Icon=/tmp/ECC-certis/Images/CEH.png" >> CEH.desktop
+	echo "Terminal=true" >> CEH.desktop
+	echo "Type=Application" >> CEH.desktop
+	echo "Categories=Utility;Application;" >> CEH.desktop
 	chmod +x CEH.desktop
+	echo "CEH Icon created"
 }
 
 # Remove default Icons from the Panel and setting custom icons in panel
 customize_default_icons() {
-gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop','application://firefox.desktop']"
+gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop','application://firefox.desktop', 'application://gnome-terminal.desktop', 'application://CEH.desktop']"
 }
 
 #rm -f /tmp/ECC-certis
@@ -49,7 +48,7 @@ gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop','
 
 echo "Welcome to EC-Council OS Suite"
 install_desktop_background
-#creating_custom_desktop_icons
+creating_custom_desktop_icons
 customize_default_icons
 reboot
 
