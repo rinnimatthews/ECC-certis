707386a13a093197dc5432e63ae1cde3766e5e22
rinnimatthews <rinnimatthews@gmail.com>
2017-02-08 22:32:15 -0800

    Customization

--- main_dist.sh
+++ main_dist.sh
@@ -49,7 +49,7 @@ creating_custom_desktop_icons() {
 # Remove default Icons from the Panel and setting custom icons in panel
 customize_default_icons() {
 echo "Replacing default panel icons"
-sudo gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop','application://firefox.desktop', 'application://gnome-terminal.desktop', 'application://CEH.desktop']"
+sudo gsettings set com.canonical.Unity.Launcher favorites "['application://CEH.desktop', 'application://gnome-terminal.desktop', 'nautilus-home.desktop','application://firefox.desktop']"
 echo "default panel icons replaced"
 }
 
