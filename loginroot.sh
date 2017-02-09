1597ba051597da97c76114789c2873b46adaa374
rinnimatthews <rinnimatthews@gmail.com>
2017-02-09 02:20:45 -0800

    loginroot

--- loginroot.sh
+++ loginroot.sh
@@ -1,6 +1,7 @@
 # Login as root
 login_root(){
-sudo passwd
-echo "eccouncil" | sudo -S
+#sudo passwd
+#eccouncil
+#echo "eccouncil" | sudo -S
 echo "greeter-show-manual-login=true" >> /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
 }
