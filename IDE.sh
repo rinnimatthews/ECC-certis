0747ba52651c9e0ea0b0014eab30046656bb96b7
rinnimatthews <rinnimatthews@gmail.com>
2017-02-12 23:23:04 -0800

    GNOME

--- IDE.sh
+++ IDE.sh
@@ -1,9 +1,9 @@
 install_gnome(){
-sudo apt-get remove lightdm && sudo apt-get install gdm
+sudo apt-get remove lightdm -y && sudo apt-get install gdm -y
 #sudo service gdm start
-sudo apt-get install gnome-shell --no-install-recommends
-sudo apt-get install ubuntu-gnome-desktop
-sudo apt-get remove unity
+sudo apt-get install gnome-shell -y
+sudo apt-get install ubuntu-gnome-desktop -y
+sudo apt-get remove unity -y
 }
 
 install_gnome
