aa60755f2ff42557e11eddd56fec09280acf6407
rinnimatthews <rinnimatthews@gmail.com>
2018-02-02 11:02:54 +0530

    Update main_dist.sh

--- main_dist.sh
+++ main_dist.sh
@@ -95,10 +95,10 @@ echo "default panel icons replaced"
 }
 
 # Login as root
-login_root(){
-sudo passwd
-eccouncil
-echo "greeter-show-manual-login=true" >> /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
+#login_root(){
+#sudo passwd
+#eccouncil
+#echo "greeter-show-manual-login=true" >> /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
 }
 
 
