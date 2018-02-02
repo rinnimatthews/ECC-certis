6aa6b6d8ff310b321fa82a76f3b0b103a2e24744
rinnimatthews <rinnimatthews@gmail.com>
2018-02-02 15:46:44 +0530

    Update CEH.sh

--- link_files/CEH.sh
+++ link_files/CEH.sh
@@ -1,1 +1,2 @@
-wget --quiet -O - https://raw.githubusercontent.com/LalithaKurma/ECCTest/master/CEHbootstrap.sh | sudo bash -s -- -i -s –y
+#wget --quiet -O - https://raw.githubusercontent.com/LalithaKurma/ECCTest/master/CEHbootstrap.sh | sudo bash -s -- -i -s –y
+wget --quiet -O - https://raw.githubusercontent.com/rinnimatthews/ECC-bootstrap/master/CEHbootstrap.sh | sudo bash -s -- -i -s –y
